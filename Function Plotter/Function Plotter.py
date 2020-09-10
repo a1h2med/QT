a891ea8c83fdf7e18d21ad6be2065ae0f6072291
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2020-09-10 02:51:26 +0200

    Update Function Plotter.py

--- Function Plotter/Function Plotter.py
+++ Function Plotter/Function Plotter.py
@@ -1,6 +1,28 @@
-# ToDo add documentation.
-# ToDo increase font size.
-# ToDo support ^.
+"""
+    This project intends to plot any function user enters.
+    this project is separated into multiple parts, the first two parts behaves as a compiler,
+    it  1- checks the syntax provided by the user, saves it,
+        2- checks the grammar provided.
+    third part is GUI which consists of:
+        1- labels           2- text editor           3- button
+        4- plot
+    fourth and final part is the part where it calculates the function and plot it.
+
+    features:
+        1- checks syntax entered by the user.
+        2- displays error messages if any.
+        3- takes min, max range for the function.
+        4- checks on the input range.
+        5- plot entered equation using matplotlib, which is embedded in PySide2 GUI.
+        6- simple design.
+
+    limitations:
+        1- it can draw equations which has only one variable.
+        2- as this application is still under development, so it supports only (+, -, *, ^, /), for now.
+        3- it can't handle (,{,[, which will be considered during future thoughts.
+
+"""
+# call required libraries
 import numpy as np
 from PySide2.QtWidgets import *
 from PySide2 import QtWidgets, QtCore
@@ -12,38 +34,70 @@ import sys
 from PySide2.QtWidgets import (QTextEdit, QPushButton, QApplication,
                                QVBoxLayout, QDialog, QGridLayout)
 
+# global variables made to save entered equation, min, max values, and error message.
 user_input_eqn = ''
 user_input_min = 0
 user_input_max = 0
+errorMessage = ''
 
+# this function is made to check whether I've nearly reached the end of the equation or not.
 def symbol_checking(symbol, function):
+    """
+
+    :param symbol: current index number of a certain equation
+    :param function: passed equation
+    :return: true when the index number less than length of equation by one, else it will return false
+    """
     if symbol < len(function) - 1:
         return True
     else:
         return False
 
 
+# A function to hold all types of errors I got.
 def Error_Handling(error_code_number):
+    """
+
+    :param error_code_number: error code.
+    :return: corresponding error message.
+    """
+    global errorMessage
     switch = {
         1: 'variable error, please enter exactly one variable',
         2: 'unsupported operator, please recheck the function',
         3: 'Expected an operator',
         4: 'Expected a digit or a variable',
         5: 'Expected a variable',
-        6: 'Make sure that Min/Max is a valid integer number'
+        6: 'Make sure that Min/Max is a valid integer number',
+        7: 'Please recheck Max/Min values'
     }
-    # ToDo: make it appears in an error dialog
-    print(switch.get(error_code_number))
+    errorMessage = switch.get(error_code_number)
 
 
+# checking syntax of the equation provided by the user.
+# here I'll check on certain things,
+# 1- operators entered      2- variables        3- integers.
 def Lexical_Analyzer(function):
+    """
+
+    :param function: equation entered by the user.
+    :return: false, whenever there's an error, else it will call parser, passing operators found,
+    digits, and variables, then returns whatever it returns.
+    """
     list_of_operators = []
     list_of_digits = []
     variable = ''
+    # this flag is made to check on whether I already have a variable stored or not.
     flag = 0
+    # what I'm doing here is very simple
+    # 1- loop over the string
+    # 2- get current char and check on it to know what it is.
+    # 3- if not found raise an error, else store it.
     for symbol in range(len(function)):
         current_char = function[symbol]
-        if current_char == '+' or current_char == '-' or current_char == '*' or current_char == '/':
+
+        if current_char == '+' or current_char == '-' or current_char == '*' or current_char == '/' \
+                or current_char == '^':
             list_of_operators.append(current_char)
         elif current_char.isalpha():
             if flag == 0:
@@ -63,6 +117,7 @@ def Lexical_Analyzer(function):
     if variable == '':
         Error_Handling(5)
         return False
+    # if there was no error call parser, and return whatever it returns.
     out = Parser_Analyzer(function, list_of_digits, list_of_operators, variable, 0)
     return out
 
@@ -83,21 +138,41 @@ def Parser_Analyzer(function, list_of_digits, list_of_operators, variable, start
         3- [variable]+
     """
     for symbol in range(len(function)):
+        
+        # get the current char, and check on it, whether it's a variable or integer, if nothing raise an error.
+        # it's clearly that it's the same steps, but I've made to steps before performing recursion (explained down)
+        # as I've seen that it was the best thing to separate the equation into three parts, if I have an equation 
+        # like: x+2+3+4, then it was easier to cut it like x+2, then pass whatever comes again to the function
+        # which should be an operator followed by an equation or a digit, like in the above example, which 
+        # in my case will be: +3, +4.
         current_symbol = function[symbol]
         if current_symbol == variable:
+            # if it's a variable, check on the string provided, then raise the current symbol to get
+            # the next char.
             out = symbol_checking(symbol, function)
             symbol += 1
+            # if symbol is not at its end, then proceed
             if out:
+                # get the current char, and check whether it's in the list of operators 
+                # provided by the lexical analyzer or not, if not check on the digits list
+                # if not raise an error.
                 current_symbol = function[symbol]
                 if current_symbol in list_of_operators:
                     out = symbol_checking(symbol, function)
                     symbol += 1
                     if out:
+                        # get the current char, check whether it's a variable or digit or raise an error.
+                        # check on the length, if it's the last, return true, 
+                        # else call the parser again, passing to it the new function, 
+                        # list of digits, operators, variable, and flag which indicates whether it's the 
+                        # first call or not, as it's not so I need it to get into the (operator part).
                         current_symbol = function[symbol]
                         if current_symbol == variable:
                             if symbol == len(function) - 1:
                                 return True
                             else:
+                                
+                                # pass unchecked part of the equation.
                                 function = function[symbol + 1:]
                                 output = Parser_Analyzer(function, list_of_digits, list_of_operators, variable, 1)
                                 return output
@@ -119,9 +194,15 @@ def Parser_Analyzer(function, list_of_digits, list_of_operators, variable, start
                     Error_Handling(3)
                     return False
             else:
+                # ToDo support one variable input.
                 Error_Handling(3)
                 return out
+        # operator part
         elif starting_flag != 0:
+            # check on symbol.
+            # check on length.
+            # get next char.
+            # check on its type.
             if current_symbol in list_of_operators:
                 out = symbol_checking(symbol, function)
                 symbol += 1
@@ -159,6 +240,8 @@ def Parser_Analyzer(function, list_of_digits, list_of_operators, variable, start
                         return out
             else:
                 return True
+        
+        # digit part, same as variable part
         elif ord(current_symbol[0]) - 48 in range(0, 9):
             if int(current_symbol) in list_of_digits:
                 out = symbol_checking(symbol, function)
@@ -204,34 +287,65 @@ def Parser_Analyzer(function, list_of_digits, list_of_operators, variable, start
     if len(function) == 0:
         return True
 
+# check on min max values provided by the user.
+def MinMaxChecking(input_string):
+    """
 
+    :param input_string: min/max value
+    :return: int value of min/max, or empty string
+    """
+
+    """
+        1- it will loop on provided string.
+        2- checks on the ASCII code of each char of it
+        3- raise an error if there were any, else return int of string 
+    """
+    flag = 1
+    for i in range(len(input_string)):
+        if ord(input_string[i]) - 48 in range(0, 9):
+            continue
+        else:
+            flag = 0
+            Error_Handling(6)
+    if flag:
+        return int(input_string)
+    else:
+        return ''
+
+# checks on the inputs provided by the user.
 def Input_Checker(function, minimum_value, maximum_value):
+    """
+
+    :param function: input equation
+    :param minimum_value: input min
+    :param maximum_value: input max
+    :return: out => output of compiler, which is true if the equation syntax and grammar was correct,
+     else otherwise.
+     input_min => checked min value integer.
+     input_max => checked max value integer.
+    """
     out = Lexical_Analyzer(function)
     input_min = MinMaxChecking(minimum_value)
     input_max = MinMaxChecking(maximum_value)
     return out, input_min, input_max
 
-def MinMaxChecking(input_string):
-    for i in range(len(input_string)):
-        if ord(input_string[i]) - 48 in range(0, 9):
-            continue
-        else:
-            Error_Handling(6)
-    return int(input_string)
-
+# this class which holds main GUI.
+# it's consists of multiple widgets lays in grid layout which puts them just as needed.
 class FunctionPlotter(QDialog):
 
     def __init__(self, parent=None):
         super(FunctionPlotter, self).__init__(parent)
         self.mainwindow()
 
-    # Greets the user
+    # main window GUI
     def mainwindow(self):
 
+        # setting window title, and setting layout of GUI to be in shape of grid.
         self.setWindowTitle("Function Plotter")
         grid = QGridLayout()
         self.setLayout(grid)
 
+        # made three labels to let the user know which goes where.
         eqn_title = QLabel("Please Insert Your Equation Here")
         min_title = QLabel("Please Insert Min Value")
         max_title = QLabel("Please Insert Max Value")
@@ -240,12 +354,19 @@ class FunctionPlotter(QDialog):
         min_title.setAlignment(QtCore.Qt.AlignHCenter)
         max_title.setAlignment(QtCore.Qt.AlignHCenter)
 
+        # three text editors, and I've set font size to 20, to make it clearer.
         self.eqn_text_edit = QTextEdit()
         self.min_text_edit = QTextEdit()
         self.max_text_edit = QTextEdit()
 
+        self.eqn_text_edit.setFontPointSize(20)
+        self.min_text_edit.setFontPointSize(20)
+        self.max_text_edit.setFontPointSize(20)
+
+        # push button made to let the user submit his/her inputs.
         submit = QtWidgets.QPushButton("submit", self)
 
+        # adding previous widgets to grid, where everything should goes just where I need it.
         grid.setSpacing(10)
         grid.addWidget(eqn_title, 0, 0, 1, 1)
         grid.addWidget(self.eqn_text_edit, 1, 0, 1, 1)
@@ -255,10 +376,12 @@ class FunctionPlotter(QDialog):
         grid.addWidget(self.max_text_edit, 1, 2, 1, 1)
         grid.addWidget(submit, 2, 0, 1, 1)
 
+        # on clicking the button, call this function.
         submit.clicked.connect(self.connector)
         self.show()
 
     def connector(self):
+        # save all the entered text, pass them to input checker to check on the values, get new values.
         global user_input_eqn
         global user_input_min
         global user_input_max
@@ -267,15 +390,31 @@ class FunctionPlotter(QDialog):
         user_input_max = self.max_text_edit.toPlainText()
 
         out, user_input_min, user_input_max = Input_Checker(user_input_eqn, user_input_min, user_input_max)
-        if out:
+
+        # check on the outputs of input checker, if there's anything wrong, just display an error message.
+        # if nothing wrong, call other class, where the calculation lays in.
+        if out and (user_input_min != '') and (user_input_max != '') and (user_input_max > user_input_min):
             self.w = Window()
             self.w.show()
             self.hide()
         else:
-            print(2)
+            global errorMessage
+            if user_input_max <= user_input_min:
+                Error_Handling(7)
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Error")
+            msg.setInformativeText(errorMessage)
+            msg.setWindowTitle("Error")
+            msg.exec_()
+            msg.hide()
 
 class Window(QDialog):
 
+    # making a plot.
+    # first, I've made a figure, then called Figure canvas, passed this figure to it.
+    # then made navigation tool bar, passed created canvas to it.
+    # added all of that to the layout, which is Vbox.
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
         self.figure = plt.figure()
@@ -293,6 +432,9 @@ class Window(QDialog):
         global user_input_min
         global user_input_max
         self.figure.clear()
+
+        # before plotting, replace every ^ with **, to server the correct function.
+        user_input_eqn.replace("^", "**")
         x = np.array(range(user_input_min, user_input_max))
         y = eval(user_input_eqn)
         ax = self.figure.add_subplot(111)
@@ -301,10 +443,6 @@ class Window(QDialog):
 
 
 if __name__ == '__main__':
-    # Create the Qt Application
     app = QApplication(sys.argv)
-    # Create and show the form
     Plotter = FunctionPlotter()
-#    Plotter.show()
-    # Run the main Qt loop
     sys.exit(app.exec_())
